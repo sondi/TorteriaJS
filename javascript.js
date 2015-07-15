ef9575b47070ff07888db3e646fc4359404a9409
Sandra Tirado <Sondi@Sandras-MacBook-Pro.local>
2015-07-15 12:09:03 -0500

    Initialize method and bake_time method

--- javascript.js
+++ javascript.js
@@ -3,6 +3,32 @@ $( document ).ready(function() {
     event.preventDefault();
     var type = $("input[name='type']").val();
     var time = $("input[name='time']").val();
-  });
+
+
+    var new_torta = new Torta(type);
+    console.log(new_torta);
+    var result = new_torta.bake_time();
+    console.log(result);
+});
+
+var Torta = function(type){
+		var self = this;
+	function initialize(){
+		self.type = type;
+	};
+	initialize();
+
+};	
+	Torta.prototype.bake_time = function(){
+		var bake_time = {"jamon": 3, "queso": 8, "milanesa": 10};
+		return bake_time[this.type]
+	};
+
+
+var Oven = function(time){
+	var time_baked = 0;
+	this.time = time;
+};
+
 
 });
