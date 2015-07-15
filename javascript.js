338f36e4397cbc1831e8762a3870424e31641841
Mauricio García <m4ur1c1o.gg@gmail.com>
2015-07-15 13:41:53 -0500

    Add TortaBatch class with initialize method

--- javascript.js
+++ javascript.js
@@ -6,29 +6,63 @@ $( document ).ready(function() {
 
 
     var new_torta = new Torta(type);
-    console.log(new_torta);
+    // console.log(new_torta);
     var result = new_torta.bake_time();
-    console.log(result);
+    // console.log(result);
+    var batch = new TortaBatch(8, type);
+    console.log(batch);
+  });
 });
 
+// Class Torta
 var Torta = function(type){
-		var self = this;
+	var self = this;
 	function initialize(){
 		self.type = type;
 	};
 	initialize();
+};
 
-};	
-	Torta.prototype.bake_time = function(){
-		var bake_time = {"jamon": 3, "queso": 8, "milanesa": 10};
-		return bake_time[this.type]
-	};
+// bake_time method for Torta
+Torta.prototype.bake_time = function(){
+	var bake_time = {"jamon": 3, "queso": 8, "milanesa": 10};
+	return bake_time[this.type];
+};
+///////////////////////////////////////////////////////////////////////////////
 
+// Class TortaBatch
+var TortaBatch = function(batch_size, type){
+  var self = this;
+  function initialize(){
+    self.batch_size = batch_size;
+    self.type = type;
+    var tortas = [];
+    // console.log(tortas);
+    for (var i = 0; i < batch_size; i++) {
+      tortas.push(new Torta(type));
+    };
+    // console.log(tortas);
+    var ready_time = tortas[0].bake_time();
+    var cook_time = 0;
+  };
+  initialize();
+};
 
-var Oven = function(time){
-	var time_baked = 0;
-	this.time = time;
+// status method for TortaBatch
+TortaBatch.prototype.status = function(){
+
 };
 
 
-});
+
+
+
+
+
+
+// Class Oven
+// var Oven = function(time){
+// 	var time_baked = 0;
+// 	this.time = time;
+// };
+///////////////////////////////////////////////////////////////////////////////
